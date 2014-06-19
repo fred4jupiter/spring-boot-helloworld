7e295d4df316e9970f1f1445901ce562b9b3c06c
fred4jupiter <hamsterhase@gmx.de>
2014-06-19 13:16:27 +0200

    update for profiles

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,8 @@
 package hello;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -14,10 +17,18 @@ public class GreetingController {
 	@Autowired
 	private Environment environment;
 
+	@RequestMapping(value = "/")
+	public String index() {
+		return "redirect:greeting";
+	}
+
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
 		model.addAttribute("name", name);
 		model.addAttribute("activeProfiles", StringUtils.arrayToCommaDelimitedString(environment.getActiveProfiles()));
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		model.addAttribute("dateTime", formatter.format(LocalDateTime.now()));
 		return "greeting";
 	}
 
